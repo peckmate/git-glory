4b616eac91a33e2061af7ff7025f101bdd08185f
Tyler Peck <tpeck@costar.com>
2023-08-15 15:22:29 -0700

    make it count the commits

--- Program.cs
+++ Program.cs
@@ -1,31 +1,38 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using LibGit2Sharp;
+using Libgit2Sharp;
+using System.Collections.Generic;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-       var repositoryPath = Console.ReadLine();
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Enter the path to the repository: ");
+		string repositoryPath = Console.Readline();
 
-        using (var repo = new Repository(repositoryPath))
-        {
-            var authorNames = new List<string>();
+		using (var repo = new Repository(repositoryPath));
+		{
+			var authorCommitCounts = new Dictionary<string, int>();
 
-            foreach (var commit in repo.Commits)
-            {
-                authorNames.Add(commit.Author.Name);
-            }
+			foreach (var commit in repo.Commits)
+			{
+				if (authorCommitCounts.ContainsKey(authorName))
+				{
+					authorCommitCounts[authorName]++;
+				}
+				else
+				{
+					authorCommitCounts[authorName] = 1;
+				}
+			}
 
-            var uniqueAuthorNames = authorNames.Distinct();
+			Console.WriteLine("Author Commit Counts:");
 
-            Console.WriteLine("Unique Author Names:");
-
-            foreach (var authorName in uniqueAuthorNames)
-            {
-                Console.WriteLine(authorName);
-            }
-        }
-    }
+			foreach (var kvp in authorCommitCounts)
+			{
+				console.WriteLine($"{kvp.Key}: {kvp.Value} commits");
+			}
+		}
+	}
 }
