22648d47503f3652b7909a190e5495122ba044b0
Tyler Peck <tpeck@costar.com>
2023-08-15 15:15:08 -0700

    m1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Enter the path to the repository: ");
+    var repositoryPath = Console.ReadLine();
+    using (var repo = new Repository(repositoryPath))
+    {
+      var authorNames = new List<string>();
+
+      foreach (var commit in repo.Commits)
+      {
+        authorNames.Add(commit.Author.Name);
+      }
+
+      var uniqueAuthorNames = authorNames.Distinct();
+
+      Console.WriteLine*("Unique Author Names:");
+      foreach (var authorName in uniqueAuthorNames)
+      {
+        Console.WriteLine(authorName);s
+      }
+    }
+  }
+}
