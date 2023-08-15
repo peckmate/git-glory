1c57943a295efe9e03371be62111179d2cce9b2e
Tyler Peck <peckmate@users.noreply.github.com>
2023-08-15 15:23:10 -0700

    spaces vs tabs

--- Program.cs
+++ Program.cs
@@ -6,33 +6,33 @@ using System.Collections.Generic;
 
 public class Program
 {
-	public static void Main(string[] args)
-	{
-		Console.WriteLine("Enter the path to the repository: ");
-		string repositoryPath = Console.Readline();
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the path to the repository: ");
+        string repositoryPath = Console.Readline();
 
-		using (var repo = new Repository(repositoryPath));
-		{
-			var authorCommitCounts = new Dictionary<string, int>();
+        using (var repo = new Repository(repositoryPath));
+        {
+            var authorCommitCounts = new Dictionary<string, int>();
 
-			foreach (var commit in repo.Commits)
-			{
-				if (authorCommitCounts.ContainsKey(authorName))
-				{
-					authorCommitCounts[authorName]++;
-				}
-				else
-				{
-					authorCommitCounts[authorName] = 1;
-				}
-			}
+            foreach (var commit in repo.Commits)
+            {
+                if (authorCommitCounts.ContainsKey(authorName))
+                {
+                    authorCommitCounts[authorName]++;
+                }
+                else
+                {
+                    authorCommitCounts[authorName] = 1;
+                }
+            }
 
-			Console.WriteLine("Author Commit Counts:");
+            Console.WriteLine("Author Commit Counts:");
 
-			foreach (var kvp in authorCommitCounts)
-			{
-				console.WriteLine($"{kvp.Key}: {kvp.Value} commits");
-			}
-		}
-	}
+            foreach (var kvp in authorCommitCounts)
+            {
+                console.WriteLine($"{kvp.Key}: {kvp.Value} commits");
+            }
+        }
+    }
 }
