bccc87170c2974f99f5238db05fd2d552f97d272
peckmate <peckmate@users.noreply.github.com>
2023-08-15 17:20:24 -0700

    add a menu, hopefully

--- Program.cs
+++ Program.cs
@@ -1,52 +1,80 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using LibGit2Sharp;
+﻿using System; 
+using System.IO; 
+using System.Linq; 
+using LibGit2Sharp; 
 using System.Collections.Generic;
 using ConsoleTableExt;
+using System.ComponentModel.Design;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Give me a path");
-        var repositoryPath = Console.ReadLine();
+public class Program 
+{ 
+    public static void Main(string[] args) 
+    { 
+        while (true) 
+        { 
+            Console.WriteLine("Menu:"); 
+            Console.WriteLine("1. Count Changes"); 
+            Console.WriteLine("0. Exit"); 
+            Console.Write("Select an option: "); 
 
+            int option = int.Parse(Console.ReadLine());
+
+            switch(option)
+            { 
+                case 1: 
+                    Console.WriteLine("Give me a path");
+                    var repositoryPath = Console.ReadLine(); 
+                    CountChanges(repositoryPath); 
+                    break; 
+                
+                case 0: 
+                    return; 
+
+                default: 
+                    Console.WriteLine("Invalid option. Try again."); 
+                    break; 
+            }
+        }
+    }
+
+    public static void CountChanges(string repositoryPath) 
+    { 
         using (var repo = new Repository(repositoryPath))
-        {
-            var authorChanges = new Dictionary<string, (int additions, int deletions)>();
+        { 
+            var authorChanges = new Dictionary<string, (int additions, int deletions)>(); 
 
-            foreach (var commit in repo.Commits)
-            {
-                var changes = commit.Parents.Any()
+            foreach (var commit in repo.Commits) 
+            { 
+                var changes = commit.Parents.Any() 
                     ? repo.Diff.Compare<Patch>(commit.Parents.First().Tree, commit.Tree)
-                    : repo.Diff.Compare<Patch>(null, commit.Tree);
+                    : repo.Diff.Compare<Patch>(null, commit.Tree); 
+                
+                string authorName = commit.Author.Name; 
 
-                string authorName = commit.Author.Name;
-
-                int additions = changes.Sum(change => change.LinesAdded);
-                int deletions = changes.Sum(change => change.LinesDeleted);
+                int additions = changes.Sum(change => change.LinesAdded); 
+                int deletions = changes.Sum(change => change.LinesDeleted); 
 
                 if (authorChanges.ContainsKey(authorName))
-                {
-                    var (prevAdditions, prevDeletions) = authorChanges[authorName];
-                    authorChanges[authorName] = (prevAdditions + additions, prevDeletions + deletions);
+                { 
+                    var (prevAdditions, prevDeletions) = authorChanges[authorName]; 
+                    authorChanges[authorName] = (prevAdditions + additions, prevDeletions + deletions); 
                 }
-                else
-                {
-                    authorChanges[authorName] = (additions, deletions);
+                else 
+                { 
+                    authorChanges[authorName] = (additions, deletions); 
+
                 }
             }
 
-            var tableData = new List<List<object>>();
+            var tableData = new List<List<object>>(); 
 
-            foreach (var kvp in authorChanges)
-            {
-                var row = new List<object>();
-                row.Add(kvp.Key);
-                row.Add(kvp.Value.additions);
+            foreach (var kvp in authorChanges) 
+            { 
+                var row = new List<object>(); 
+                row.Add(kvp.Key); 
+                row.Add(kvp.Value.additions); 
                 row.Add(kvp.Value.deletions);
-                tableData.Add(row);
+                tableData.Add(row); 
             }
 
             ConsoleTableBuilder
@@ -54,6 +82,7 @@ public class Program
                 .WithTitle("Ch-ch-changes", ConsoleColor.Yellow, ConsoleColor.DarkGray)
                 .WithColumn("Name", "Additions", "Deletions")
                 .ExportAndWriteLine();
+
         }
     }
 }
